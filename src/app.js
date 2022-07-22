a372b48b57979657f257eb5b1c5740af9a7f4a84
Maryna Vorobei <vorobeymrn@gmail.com>
2022-07-23 00:45:24 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
- 
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "e5ea83abd51b392f1217843d2e96ea3e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
